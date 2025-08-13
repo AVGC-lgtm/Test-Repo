bb83f15e89b23df428cb74adabb7205eb08f20d0
AVGC-lgtm <ai.avgc@wildrexsolutions.com>
2025-08-13 15:43:01 +0530

    Delete client directory

--- client/next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ['http://192.168.1.3:8005'],
-  },
-};
-
-module.exports = nextConfig;
--- client/src/utils/logout.ts
+++
@@ -1,7 +0,0 @@
-export function logout() {
-  // Remove token from localStorage
-  localStorage.removeItem('token');
-
-  // Redirect to signin page
-  window.location.href = '/auth/signin';
-}
